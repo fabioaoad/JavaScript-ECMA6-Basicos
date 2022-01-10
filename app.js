55c0ea5b323f5888a8193725308528c16822d7ac
Fabio <fabioaoad@gmail.com>
2022-01-10 12:58:54 -0300

    Inicio de Proyecto

---
+++ app.js
@@ -0,0 +1,95 @@
+// const IMPUESTO_SV = 15.23;
+//
+// if (true) {
+//     const IMPUESTO_SV = 19.2;
+//     console.log(IMPUESTO_SV);
+// }
+// console.log(IMPUESTO_SV);
+//
+//
+// const PERSONA = {
+//     nombre: "Fabio",
+//     apellido: "Aoad"
+// };
+//
+// //PERSONA.nombre = "Martin";
+//
+// PERSONA = {
+//     nombre: "Juan",
+//     apellido: "Perez"
+// };
+//
+// console.log(PERSONA);
+
+// for (let i = 0; i <= 10; i++) {
+//   //
+//   //
+// }
+// console.log(i);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var funciones = [];
+//
+// for (var i = 0; i < 10; i++) {
+//     funciones.push(
+//       (function(valor){
+//         return function(){
+//           console.log(valor);
+//         }
+//       })(i)
+//     );
+// }
+//
+//
+//
+// funciones.forEach( function (func){
+//   func();
+// });
+
+
+
+
+//funciones.push( function(){console.log(i);} );
+
+// let numero = 10;
+// if( true ){
+//   let numero = 20;
+//   console.log('valor if: ', numero);
+//     while(true){
+//       let numero = 20;
+//       console.log('valor while: ', numero);
+//       for(let numero = 1; numero < 10; numero ++){
+//         numero = numero;
+//         console.log('valor de for: ', numero);
+//       } break;
+//     }
+// }
+// console.log('valor es: ',numero);
+
+
+
+
+
+
+
+var saludo = "Hola Mundo!";
+console.log( saludo.substr(0,1) === "H" );
+console.log( saludo.substr(0,1) === "h" );
+
+console.log( saludo.startsWith("H") );
+console.log( saludo.startsWith("h") );
+
+
+console.log( saludo.endsWith("!") );
