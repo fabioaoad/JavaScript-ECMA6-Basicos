8f302e2bb8a1e5fe1ffbd9c0fc760bbb31285671
Fabio <fabioaoad@gmail.com>
2022-01-15 18:03:47 -0300

    Métodos concisos (tambien llamados simples)

--- app5.js
+++ app5.js
@@ -38,3 +38,42 @@ var martin = crearPersona("Martin", "Aoad", "26")
 console.log( martin );
 
 // EJEMPLO2 ECMAS6 - FIN
+
+
+
+
+console.log("////////////////////////////////////////////");
+////////////////////////////////////////////
+// Métodos concisos (tambien llamados simples)
+
+// EJEMPLO1 - ECMAS5 - INICIO
+
+var  persona = {
+  nombre: "Fabio",
+  getNombre: function(){
+    console.log( this.nombre );
+  }
+}
+
+
+persona.getNombre();
+
+
+// EJEMPLO1 - ECMAS6 - FIN
+
+
+
+// EJEMPLO2 - ECMAS6 - INICIO
+
+var  persona2 = {
+  nombre: "Fabio",
+  getNombre(){
+    console.log( this.nombre );
+  }
+}
+
+
+persona2.getNombre();
+
+
+// EJEMPLO2 - ECMAS6 - FIN
