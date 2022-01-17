829994def75bf6c3b7c9f700ea3ab57f4e81438f
Fabio <fabioaoad@gmail.com>
2022-01-17 11:26:29 -0300

    Destructuración de objetos anidados

--- app6.js
+++ app6.js
@@ -124,3 +124,50 @@ console.log( nombre, email, facebook, google, dePago, twitter, otraCosa, cuentaG
 
 
 // EJEMPLO1 ECMAS6 - FIN
+
+
+
+
+
+
+
+
+console.log("////////////////////////////////////////////");
+////////////////////////////////////////////
+// Destructuración de objetos anidados
+
+let autoGuardado = {
+
+  archivo: "app.js",
+  cursor: {
+    linea: 7,
+    columna: 16
+  },
+  ultimoArchivo:{
+    archivo: "index.html",
+    cursor: {
+              cursor: 8,
+              columna: 20
+            }
+  },
+  otroNodo:{
+            subNodo:{
+                     cursor:{
+                              linea: 11,
+                              columna: 11
+                            }
+                    }
+          }
+};
+
+let { cursor: cursorActivo } = autoGuardado;
+console.log( cursorActivo );
+
+let { ultimoArchivo: { cursor:ultimoArchivo } } = autoGuardado;
+console.log( ultimoArchivo );
+
+let { otroNodo: { subNodo:{ cursor:otroSuperNodo } } } = autoGuardado;
+console.log( otroSuperNodo );
+
+let otroSuperNodo2 = autoGuardado.otroNodo.subNodo.cursor;
+console.log( otroSuperNodo2 );
