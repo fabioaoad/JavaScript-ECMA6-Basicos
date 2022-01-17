2df150de4b57afcce72c0b212e3ecfbbd00d7ba7
Fabio <fabioaoad@gmail.com>
2022-01-17 11:13:23 -0300

    Destructuración de objetos

--- app6.js
+++ app6.js
@@ -29,12 +29,97 @@ console.log( Object.getPrototypeOf( angora ) === gato );
 angora.sonido();
 angora.chillido();
 
-// convierto mi gato angora a un perro con "setPrototypeOf"
+// convierto mi gato angora a un perro con "setPrototypeOf" - HERENCIA
 // nota: el chillido no funciona porq el perro no lo tiene
 Object.setPrototypeOf( angora, perro );
 console.log( Object.getPrototypeOf( angora ) === gato );
 
 angora.sonido();
-angora.chillido();
+//angora.chillido();
+
+// EJEMPLO1 ECMAS6 - FIN
+
+
+
+
+
+
+
+
+console.log("////////////////////////////////////////////");
+////////////////////////////////////////////
+// Acceso al prototipo con la referencia SUPER
+
+
+
+// EJEMPLO1 ECMAS5 - INICIO
+
+var persona = {
+  saludar(){
+    return "Hola";
+  }
+};
+
+
+
+var amigo = {
+  saludar(){
+    return Object.getPrototypeOf(this).saludar.call(this) + ", saludos ECMS5!!!";
+  }
+};
+
+// heredo las propiedades de "persona" y se las paso a "amigo"
+Object.setPrototypeOf( amigo, persona );
+console.log( amigo.saludar() );
+
+// EJEMPLO1 ECMAS5 - FIN
+
+console.log("////////////////////////////////////////////");
+
+
+// EJEMPLO2 ECMAS6 - INICIO
+
+var persona2 = {
+  saludar(){
+    return "Hola";
+  }
+};
+
+
+
+var amigo2 = {
+  saludar(){
+    return super.saludar() + ", saludos ECMS6!!!";
+  }
+};
+
+// heredo las propiedades de "persona" y se las paso a "amigo"
+Object.setPrototypeOf( amigo2, persona2 );
+console.log( amigo2.saludar() );
+
+// EJEMPLO2 ECMAS6 - FIN
+
+
+
+
+
+console.log("////////////////////////////////////////////");
+////////////////////////////////////////////
+// Destructuración de objetos
+
+// EJEMPLO1 ECMAS6 - INICIO
+
+let ajustes = {
+  nombre: "Fabio Aoad",
+  email: "fabioaoad@gmail.com",
+  facebook: "fb/fabioaoad",
+  google: "fabioaoad",
+  premium: true
+};
+
+let { nombre, facebook, email, google, premium:dePago, twitter, otraCosa = "otro valor" } = ajustes;
+
+console.log( nombre, email, facebook, google, dePago, twitter, otraCosa );
+
 
 // EJEMPLO1 ECMAS6 - FIN
