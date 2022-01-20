de5eadaa98302c164a09dcb1a4e85f6946d3e738
Fabio <fabioaoad@gmail.com>
2022-01-20 18:38:45 -0300

    Promesas - más ejemplos

--- app12.js
+++ app12.js
@@ -43,10 +43,10 @@ function reject(){
 
 console.log("////////////////////////////////////////////");
 ////////////////////////////////////////////
-// Promesas
+// Promesas - más ejemplos
 
 
-// EJEMPLO1 - INICIO
+// EJEMPLO1 - ECMAS6 - INICIO
 
 function tareaAsincrona2(){
 
@@ -74,7 +74,54 @@ tareaAsincrona2().then( function(){
 console.log("Código secuencial");
 
 
-// EJEMPLO1 - FIN
+// EJEMPLO1 - ECMAS6 - FIN
+
+
+console.log("////////////////////////////////////////////");
+
+
+
+// EJEMPLO2 - ECMAS5 - INICIO
+
+function msgAfterTimeout (msg, who, timeout, onDone) {
+    setTimeout(function () {
+        onDone(msg + " Hola " + who + "!");
+    }, timeout);
+}
+
+msgAfterTimeout(" Este es un msj del ECMAS5 ", "FABIO", 3000, function (msg) {
+    msgAfterTimeout(msg, "MARTIN", 4000, function (msg) {
+        console.log("Hecho despues de 3000ms:" + msg);
+    });
+});
+
+// EJEMPLO2 - ECMAS5 - FIN
+
+
+
+// EJEMPLO3 - ECMAS6 - INICIO
+
+function msgAfterTimeout2 (msg, who, timeout) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => resolve(`${msg} Hola ${who}!`), timeout)
+    })
+}
+
+msgAfterTimeout2(" Este es un msj del ECMAS6 ", "FABIO", 5000).then((msg) =>
+    msgAfterTimeout2(msg, "MARTIN", 6000)
+).then((msg) => {
+    console.log(`Hecho despues de 5000ms:${msg}`)
+})
+
+// EJEMPLO3 - ECMAS6 - FIN
+
+
+
+
+
+
+
+
 
 
 
